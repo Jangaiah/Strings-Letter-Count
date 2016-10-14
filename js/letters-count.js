f8166283642b55c29645b4f9c73cbecc1749d5dd
Jangaiah <jangaiah2paneti@gmail.com>
2016-10-14 15:11:39 +0530

    Add files via upload

---
+++ js/letters-count.js
@@ -0,0 +1,19 @@
+//By default it gives case sensitive results.
+//If you are expecting case insensitive just pass 'false' as parameter
+"use strict";
+String.prototype.lettersCount = function(){
+ var self=this.trim();
+ if(arguments[0]==false) self=self.toLowerCase();
+ let len=self.length;
+ var countObj = {};
+ countObj[self[0]] = 1;
+ for(let i=1;i<len;i++) {
+	if(self[i]!=' '){
+		if(countObj[self[i]]) ++countObj[self[i]];
+		else countObj[self[i]]=1;
+	} 
+ } 
+ var retStr = "";
+ for(let x in countObj) retStr += x+ "="+countObj[x]+" "; 
+ return retStr;
+}
